82ca20464905992e5555b7c095d56f701f28c8cb
guneyizol <guneyizol@gmail.com>
2023-04-24 23:23:25 +0300

    fix

--- node.py
+++ node.py
@@ -12,9 +12,10 @@ myname = 'name'
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
-s.close()
 
 myip = s.getsockname()[0]
+
+s.close()
 
 broadcast_domain = myip[:myip.rfind('.')]
 
